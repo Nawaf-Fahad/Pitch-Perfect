613f60962079eb08be2d3476b32ec753e2bce720
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2023-02-02 20:23:45 +0300

    Update RecordSoundsViewController.swift

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -10,48 +10,44 @@ import AVFoundation
 
 class RecordSoundsViewController: UIViewController , AVAudioRecorderDelegate{
     var audioRecorder: AVAudioRecorder!
-    @IBOutlet weak var StopButton: UIButton!
-    @IBOutlet weak var RecordLabel: UILabel!
-    @IBOutlet weak var RecordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        StopButton.isEnabled = false
+        stopButton.isEnabled = false
     }
-
+    
     @IBAction func recordAudio(_ sender: UIButton) {
         print("Record button Was Pressed")
-        RecordLabel.text = "Recording in Progress"
-        StopButton.isEnabled = true
-        RecordButton.isEnabled = false
+        configueUI(true)
         
         
         let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
-               let recordingName = "recordedVoice.wav"
-               let pathArray = [dirPath, recordingName]
-               let filePath = URL(string: pathArray.joined(separator: "/"))
-
-               let session = AVAudioSession.sharedInstance()
-               try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
-
-               try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
-               audioRecorder.delegate = self
-               audioRecorder.isMeteringEnabled = true
-               audioRecorder.prepareToRecord()
-               audioRecorder.record()
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     @IBAction func stopRecording(_ sender: UIButton) {
-        RecordButton.isEnabled = true
-            StopButton.isEnabled = false
-            RecordLabel.text = "Tap to Record"
-            audioRecorder.stop()
-            let audioSession = AVAudioSession.sharedInstance()
-            try! audioSession.setActive(false)
+        configueUI(false)
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
     
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
@@ -61,7 +57,7 @@ class RecordSoundsViewController: UIViewController , AVAudioRecorderDelegate{
             print("Recording was not successful")
         }
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "stopRecording"{
             let playSoundsVC = segue.destination as! PlaySoundsViewController
@@ -69,5 +65,14 @@ class RecordSoundsViewController: UIViewController , AVAudioRecorderDelegate{
             playSoundsVC.recordedAudioURL = audioRecorder
         }
     }
+    
+    func configueUI(_ isRecording: Bool) {
+        stopButton.isEnabled = isRecording
+        recordButton.isEnabled = !isRecording
+        recordLabel.text = isRecording ? "Recording ..." : "Tap To Record"
+        
+    }
+    
+    
 }
 
